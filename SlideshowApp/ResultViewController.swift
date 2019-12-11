dc8c0676da6ccfdd384adb14b7080232fb22ef3d
10d132mk <10d132mk@gmail.com>
2019-12-12 02:23:07 +0900

    作成

---
+++ SlideshowApp/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  SlideshowApp
+//
+//  Created by 黒川ももこ on 2019/12/12.
+//  Copyright © 2019 10d132mk. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,86 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var sushisushi: UIImageView!
+    var timer_sec :Double=0
+    var imageIndex = 0
+    var timer: Timer!
+    let images = [UIImage(named: "fugusashi"), UIImage(named: "nigiri"), UIImage(named: "oke")]
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //最初から画像表示するためのコード
+        sushisushi.image=images[imageIndex]
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.sushi2=images[imageIndex]
+    }
+    
+    
+    @IBAction func backImage(_ sender: UIButton) {
+        if imageIndex == 0 {
+            imageIndex = 2
+        } else {
+            imageIndex -= 1
+        }
+        sushisushi.image = images[imageIndex]
     }
 
+    @IBAction func nextImage(_ sender: UIButton) {
+        if imageIndex == 2 {
+            imageIndex = 0
+            
+        } else {
+            imageIndex += 1
+        }
+        sushisushi.image = images[imageIndex]
+        
+    }
+    
+    @IBOutlet weak var modoru: UIButton!
+    @IBOutlet weak var susumu: UIButton!
 
+    
+    
+    @objc func updateTimer(_ timer: Timer) {
+        self.timer_sec = 2.0
+        if imageIndex == 2 {
+            imageIndex = 0
+            
+        } else {
+            imageIndex += 1
+            
+        }
+        sushisushi.image = images[imageIndex]
+        
+    }
+    
+    //↓から停止
+    func stopTimer() {
+        self.timer.invalidate()// タイマーを停止する
+        self.timer = nil
+        
+    }
+    @IBAction func startStop(_ sender: UIButton) {
+        modoru.isEnabled=false
+        susumu.isEnabled=false
+        //タイマーが動いてないとき
+        if self.timer == nil {
+            timer = Timer.scheduledTimer(timeInterval: 2,
+                                         target: self,
+                                         selector: #selector(updateTimer),
+                                         userInfo: nil,
+                                         repeats:true)
+            
+        } else {
+            self.timer.invalidate()// タイマーを停止する
+            self.timer = nil
+            modoru.isEnabled=true
+            susumu.isEnabled=true
+            
+        }
+    }
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
 }
-
